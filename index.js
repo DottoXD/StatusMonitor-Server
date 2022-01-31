389024ea90a255062c4ef9857075e719330c303b
DottoXD <80960175+DottoXD@users.noreply.github.com>
2022-01-31 21:07:36 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const fastify = require("fastify")({ logger: false });
 const path = require("path");
 const fetch = require("node-fetch");
-const { servers, port, fetchInterval, monitorFromUrlFile, url, grafanaSupport } = require("./config.json");
+const { serverSettings, servers, notificationSettings } = require("./config.json");
 const fs = require("fs");
 const moment = require("moment");
-const glob = require("glob")
+const glob = require("glob");
 
 let dataArray = [];
+let notificationArray = [];
 
 glob.path = __dirname;
 
@@ -17,32 +18,34 @@ fastify.register(require("fastify-static"), {
 
 fastify.register(require("./routes/index.js"));
 
-if(grafanaSupport === true) {
-    servers.forEach(server => {
-        let fixedName = server.name.replace(/\s+/g, "").toLowerCase()
-        fastify.get("/" + fixedName + "/metrics", async (request, reply) => {      
-            let data = fs.readFileSync(glob.path + "/build/json/stats.json", "utf8")
-            let parsedData = JSON.parse(data)
-            parsedData.servers.forEach(serverData => {
-                if(serverData.name === server.name) {
-                    if(serverData.online4 === false) {
-                        reply.send(`#server status\nstatus offline`)
-                    }
-                    reply.send(`#cpu usage\ncpu ${serverData.cpu}\n#ram usage\nmemory_used ${serverData.memory_used}\n#total ram\nmemory_total ${serverData.memory_total}\n#used didk\ndisk_used ${serverData.hdd_used}\n#total disk\ndisk_total ${serverData.hdd_total}\n#total swap\nswap_total ${serverData.swap_total}\n#used swap\nswap_used ${serverData.swap_used}\n#network incoming total\nnetwork_rx ${serverData.network_rx}\n#outgoing data total\nnetwork_tx ${serverData.network_tx}\n#incoming data per second\nnetwork_rx_sec ${serverData.network_rx_sec}\n#outgoing data per second\nnetwork_tx_sec ${serverData.network_tx_sec}`)
-                }
-            })
-        })
-    })
+if (serverSettings.grafanaSupport === true) {
+  servers.forEach((server) => {
+    let fixedName = server.name.replace(/\s+/g, "").toLowerCase();
+    fastify.get("/" + fixedName + "/metrics", async (request, reply) => {
+      let data = fs.readFileSync(glob.path + "/build/json/stats.json", "utf8");
+      let parsedData = JSON.parse(data);
+      parsedData.servers.forEach((serverData) => {
+        if (serverData.name === server.name) {
+          if (serverData.online4 === false) {
+            reply.send(`#server status\nstatus offline`);
+          }
+          reply.send(
+            `#cpu usage\ncpu ${serverData.cpu}\n#ram usage\nmemory_used ${serverData.memory_used}\n#total ram\nmemory_total ${serverData.memory_total}\n#used didk\ndisk_used ${serverData.hdd_used}\n#total disk\ndisk_total ${serverData.hdd_total}\n#total swap\nswap_total ${serverData.swap_total}\n#used swap\nswap_used ${serverData.swap_used}\n#network incoming total\nnetwork_rx ${serverData.network_rx}\n#outgoing data total\nnetwork_tx ${serverData.network_tx}\n#incoming data per second\nnetwork_rx_sec ${serverData.network_rx_sec}\n#outgoing data per second\nnetwork_tx_sec ${serverData.network_tx_sec}`
+          );
+        }
+      });
+    });
+  });
 }
 
-fastify.listen(port, "0.0.0.0", function (err, address) {
+fastify.listen(serverSettings.port, "0.0.0.0", function (err, address) {
   if (err) {
     fastify.log.error(err);
   }
 });
 
 const fetchAndSaveData = async function () {
-  if (monitorFromUrlFile === true)
+  if (serverSettings.monitorFromUrlFile === true)
     return fetch(url)
       .then((res) => res.json())
       .then((stringify) => JSON.stringify(stringify))
@@ -58,6 +61,34 @@ const fetchAndSaveData = async function () {
       .then((res) => res.json())
       .then((inf) => {
         dataArray.push(inf);
+        if (notificationArray.find((entry) => entry === inf.name)) {
+          if (notificationSettings.enabled === true) {
+            let requestBody = JSON.stringify({
+              embeds: [
+                {
+                  title:
+                    notificationSettings.embedName +
+                    server.name +
+                    " is back online!",
+                  description: notificationSettings.onlineDescription,
+                  color: notificationSettings.onlineColorCode,
+                },
+              ],
+            });
+
+            let requestHeaders = {
+              "Content-type": "application/json",
+            };
+
+            let req = fetch(notificationSettings.discordWebhookURL, {
+              method: "POST",
+              body: requestBody,
+              headers: requestHeaders,
+            });
+          }
+          let serviceIndex = notificationArray.indexOf(inf.name);
+          notificationArray.splice(serviceIndex);
+        }
       })
       .catch((err) => {
         let error = {
@@ -68,7 +99,37 @@ const fetchAndSaveData = async function () {
           online4: false,
           online6: false,
         };
+
         dataArray.push(error);
+        if (notificationArray.find((entry) => entry === server.name)) {
+          return;
+        } else {
+          notificationArray.push(server.name);
+          if (notificationSettings.enabled === true) {
+            let requestBody = JSON.stringify({
+              embeds: [
+                {
+                  title:
+                    notificationSettings.embedName +
+                    server.name +
+                    " is offline!",
+                  description: notificationSettings.offlineDescription,
+                  color: notificationSettings.offlineColorCode,
+                },
+              ],
+            });
+
+            let requestHeaders = {
+              "Content-type": "application/json",
+            };
+
+            let req = fetch(notificationSettings.discordWebhookURL, {
+              method: "POST",
+              body: requestBody,
+              headers: requestHeaders,
+            });
+          }
+        }
       });
   });
 
@@ -89,4 +150,4 @@ const fetchAndSaveData = async function () {
   dataArray = [];
 };
 
-setInterval(fetchAndSaveData, fetchInterval);
+setInterval(fetchAndSaveData, serverSettings.fetchInterval);
